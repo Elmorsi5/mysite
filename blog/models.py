f1a90af376b3ddf08b9d839a34fc68807053c5e1
Mohamed Elmorsi <61989626+Elmorsi5@users.noreply.github.com>
2023-01-30 20:24:53 +0200

    Defining a default sort order and add db index

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,17 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    published = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_add=True)
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields = ['-publish'])
+        ]
+    
+
